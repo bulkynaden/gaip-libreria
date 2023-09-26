a2d0407d4865b5600a56136417affe49f4af0e5d
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-09-26 20:31:04 +0200

    Cambio

--- src/main/java/es/mdef/gaip_libreria/unidades/UnidadImpl.java
+++ src/main/java/es/mdef/gaip_libreria/unidades/UnidadImpl.java
@@ -1,14 +1,13 @@
 package es.mdef.gaip_libreria.unidades;
 
 import es.mdef.gaip_libreria.actos.Acto;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-@Data
-@EqualsAndHashCode(of = {"nombre"})
+@Getter
 public class UnidadImpl implements Unidad {
     private String nombre;
     private Set<Usuario> usuarios;
@@ -49,5 +48,46 @@ public class UnidadImpl implements Unidad {
     @Override
     public void quitarInstalacion(Instalacion instalacion) {
         instalaciones.remove(instalacion);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUsuarios(Set<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public void setInstalaciones(Set<Instalacion> instalaciones) {
+        this.instalaciones = instalaciones;
+    }
+
+    public void setActos(Set<Acto> actos) {
+        this.actos = actos;
+    }
+
+    public String toString() {
+        return "UnidadImpl(nombre=" + this.getNombre() + ", usuarios=" + this.getUsuarios() + ", instalaciones=" + this.getInstalaciones() + ", actos=" + this.getActos() + ")";
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof UnidadImpl other)) return false;
+        if (!other.canEqual(this)) return false;
+        final Object this$nombre = this.getNombre();
+        final Object other$nombre = other.getNombre();
+        return Objects.equals(this$nombre, other$nombre);
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof UnidadImpl;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $nombre = this.getNombre();
+        result = result * PRIME + ($nombre == null ? 43 : $nombre.hashCode());
+        return result;
     }
 }
