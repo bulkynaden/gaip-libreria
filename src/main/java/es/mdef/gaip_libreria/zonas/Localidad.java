d1791a2e2361b57e9638aab6f8ddba0d13c012a8
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-09-26 18:48:15 +0200

    quito estado localidad

--- src/main/java/es/mdef/gaip_libreria/zonas/Localidad.java
+++ src/main/java/es/mdef/gaip_libreria/zonas/Localidad.java
@@ -1,12 +1,6 @@
 package es.mdef.gaip_libreria.zonas;
 
-import es.mdef.gaip_libreria.constantes.EstadoLocalidad;
-
 public interface Localidad {
-    EstadoLocalidad getEstado();
-
-    void setEstado(EstadoLocalidad estado);
-
     Localidad getSiguienteLocalidad();
 
     void setSiguienteLocalidad(Localidad siguienteLocalidad);
--- src/main/java/es/mdef/gaip_libreria/zonas/LocalidadImpl.java
+++ src/main/java/es/mdef/gaip_libreria/zonas/LocalidadImpl.java
@@ -1,12 +1,10 @@
 package es.mdef.gaip_libreria.zonas;
 
-import es.mdef.gaip_libreria.constantes.EstadoLocalidad;
 import lombok.Data;
 
 @Data
 public class LocalidadImpl implements Localidad {
     private Zona zona;
-    private EstadoLocalidad estado;
     private Localidad siguienteLocalidad;
     private Boolean implicaSalto;
 }
