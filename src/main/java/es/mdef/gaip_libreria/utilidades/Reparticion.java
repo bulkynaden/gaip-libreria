b17eb065f6ee1041171ae4ef63ff8b6c29c545ca
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-10-08 16:40:45 +0200

    No args contructor

--- src/main/java/es/mdef/gaip_libreria/utilidades/Reparticion.java
+++ src/main/java/es/mdef/gaip_libreria/utilidades/Reparticion.java
@@ -2,9 +2,11 @@ package es.mdef.gaip_libreria.utilidades;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Reparticion {
     private int invitacionesTribuna;
     private int invitacionesGenerica;
