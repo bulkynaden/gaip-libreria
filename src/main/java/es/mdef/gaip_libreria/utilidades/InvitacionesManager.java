9f30e8a029d6d5c960f067bd6811723254e3b779
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-10-16 08:26:24 +0200

    Corrijo errores

--- src/main/java/es/mdef/gaip_libreria/utilidades/InvitacionesManager.java
+++ src/main/java/es/mdef/gaip_libreria/utilidades/InvitacionesManager.java
@@ -4,8 +4,10 @@ import es.mdef.gaip_libreria.actos.Acto;
 import es.mdef.gaip_libreria.constantes.TipoDeZona;
 import es.mdef.gaip_libreria.invitados.Anfitrion;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Gestiona el proceso de distribución de invitaciones para un acto específico.
@@ -26,53 +28,32 @@ public final class InvitacionesManager {
      * @return Un mapa con la distribución de localidades por unidad de formación.
      */
     public static Map<String, List<List<Reparticion>>> calcularReparto(Acto acto) {
-        Map<String, List<List<Reparticion>>> distribucionPorUnidad = new HashMap<>();
+        Map<String, List<List<Reparticion>>> distribucionPorAnfitrion = new HashMap<>();
 
-        for (TipoDeZona tipo : TipoDeZona.values()) {
-            int totalInvitacionesZona = acto.getNumeroLocalidadesParaRepartirPorTipoDeZona(tipo);
+        for (String unidad : acto.getUnidadesDeFormacion()) {
+            List<Anfitrion> anfitrionesUnidad = acto.getAnfitriones().stream()
+                    .filter(a -> unidad.equals(a.getUnidadDeFormacion()))
+                    .toList();
 
-            for (String unidad : acto.getUnidadesDeFormacion()) {
-                Set<Anfitrion> anfitrionesUnidad = acto.getAnfitriones().stream()
-                        .filter(a -> unidad.equals(a.getUnidadDeFormacion()))
-                        .collect(Collectors.toSet());
-
-                int totalAnfitriones = anfitrionesUnidad.size();
-                int invitacionesPorAnfitrion = totalInvitacionesZona / totalAnfitriones;
-                int sobrantes = totalInvitacionesZona % totalAnfitriones;
-
-                List<List<Reparticion>> repartosPorAnfitriones = distribucionPorUnidad.getOrDefault(unidad, new ArrayList<>());
-                for (Anfitrion anfitrion : anfitrionesUnidad) {
-                    List<Reparticion> reparticionAnfitrion = new ArrayList<>();
-                    reparticionAnfitrion.add(new Reparticion(tipo, invitacionesPorAnfitrion));
-                    repartosPorAnfitriones.add(reparticionAnfitrion);
-                }
-                distribucionPorUnidad.put(unidad, repartosPorAnfitriones);
-
-                while (sobrantes > 0) {
-                    for (List<Reparticion> reparticionAnfitrion : repartosPorAnfitriones) {
-                        for (Reparticion r : reparticionAnfitrion) {
-                            if (r.getTipoDeZona() == tipo) {
-                                r.setNumeroDeInvitaciones(r.getNumeroDeInvitaciones() + 1);
-                                sobrantes--;
-                                if (sobrantes == 0) break;
-                            }
-                        }
-                        if (sobrantes == 0) break;
-                    }
-                }
-            }
-        }
-
-        for (List<List<Reparticion>> repartosPorAnfitriones : distribucionPorUnidad.values()) {
-            for (List<Reparticion> reparticionAnfitrion : repartosPorAnfitriones) {
+            for (Anfitrion anfitrion : anfitrionesUnidad) {
+                List<Reparticion> reparticionesPorAnfitrion = new ArrayList<>();
                 for (TipoDeZona tipo : TipoDeZona.values()) {
-                    if (reparticionAnfitrion.stream().noneMatch(r -> r.getTipoDeZona() == tipo)) {
-                        reparticionAnfitrion.add(new Reparticion(tipo, 0));
+                    int totalInvitacionesZona = acto.getNumeroLocalidadesParaRepartirPorTipoDeZona(tipo);
+                    int totalAnfitriones = anfitrionesUnidad.size();
+                    int invitacionesPorAnfitrion = totalInvitacionesZona / totalAnfitriones;
+                    int sobrantes = totalInvitacionesZona % totalAnfitriones;
+
+                    reparticionesPorAnfitrion.add(new Reparticion(tipo, invitacionesPorAnfitrion));
+
+                    if (sobrantes > 0) {
+                        reparticionesPorAnfitrion.get(reparticionesPorAnfitrion.size() - 1).setNumeroDeInvitaciones(invitacionesPorAnfitrion + 1);
+                        sobrantes--;
                     }
                 }
+                distribucionPorAnfitrion.computeIfAbsent(unidad, k -> new ArrayList<>()).add(reparticionesPorAnfitrion);
             }
         }
 
-        return distribucionPorUnidad;
+        return distribucionPorAnfitrion;
     }
 }
