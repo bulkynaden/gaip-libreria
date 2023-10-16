35c30d8d11da8982dcfc751cac81039b689c979b
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-10-16 09:17:39 +0200

    Corrijo errores

--- src/main/java/es/mdef/gaip_libreria/utilidades/InvitacionesManager.java
+++ src/main/java/es/mdef/gaip_libreria/utilidades/InvitacionesManager.java
@@ -29,26 +29,49 @@ public final class InvitacionesManager {
      */
     public static Map<String, List<Reparticion>> calcularReparto(Acto acto) {
         Map<String, List<Reparticion>> distribucionPorUnidad = new HashMap<>();
+        Map<String, Integer> cantidadAnfitrionesPorUnidad = new HashMap<>();
+
+        for (String unidad : acto.getUnidadesDeFormacion()) {
+            int count = (int) acto.getAnfitriones().stream()
+                    .filter(a -> unidad.equals(a.getUnidadDeFormacion()))
+                    .count();
+            cantidadAnfitrionesPorUnidad.put(unidad, count);
+        }
 
         for (TipoDeZona tipo : TipoDeZona.values()) {
             int totalInvitacionesZona = acto.getNumeroLocalidadesParaRepartirPorTipoDeZona(tipo);
+            Map<String, Integer> invitacionesPorUnidad = new HashMap<>();
+
+            for (String unidad : acto.getUnidadesDeFormacion()) {
+                int base = totalInvitacionesZona * cantidadAnfitrionesPorUnidad.get(unidad) / acto.getAnfitriones().size();
+                invitacionesPorUnidad.put(unidad, base);
+            }
+
+            int sobras = totalInvitacionesZona - invitacionesPorUnidad.values().stream().mapToInt(Integer::intValue).sum();
+            while (sobras > 0) {
+                for (String unidad : cantidadAnfitrionesPorUnidad.keySet().stream()
+                        .sorted((u1, u2) -> Integer.compare(cantidadAnfitrionesPorUnidad.get(u2), cantidadAnfitrionesPorUnidad.get(u1)))
+                        .toList()) {
+                    if (sobras > 0) {
+                        invitacionesPorUnidad.put(unidad, invitacionesPorUnidad.get(unidad) + 1);
+                        sobras--;
+                    } else {
+                        break;
+                    }
+                }
+            }
 
             for (String unidad : acto.getUnidadesDeFormacion()) {
                 List<Anfitrion> anfitrionesUnidad = acto.getAnfitriones().stream()
                         .filter(a -> unidad.equals(a.getUnidadDeFormacion()))
                         .toList();
 
-                int totalInvitacionesUnidad = (int) (totalInvitacionesZona * ((double) anfitrionesUnidad.size() / acto.getAnfitriones().size()));
-
-                int invitacionesPorAnfitrion = totalInvitacionesUnidad / anfitrionesUnidad.size();
-                int sobras = totalInvitacionesUnidad % anfitrionesUnidad.size();
+                int totalInvitacionesUnidad = invitacionesPorUnidad.get(unidad);
+                int basePorAnfitrion = totalInvitacionesUnidad / anfitrionesUnidad.size();
+                int sobraPorAnfitrion = totalInvitacionesUnidad % anfitrionesUnidad.size();
 
                 for (int i = 0; i < anfitrionesUnidad.size(); i++) {
-                    int invitacionesFinal = invitacionesPorAnfitrion;
-                    if (sobras > 0) {
-                        invitacionesFinal += 1;
-                        sobras--;
-                    }
+                    int invitacionesFinal = (i < sobraPorAnfitrion) ? (basePorAnfitrion + 1) : basePorAnfitrion;
                     distribucionPorUnidad.computeIfAbsent(unidad, k -> new ArrayList<>())
                             .add(new Reparticion(tipo, invitacionesFinal));
                 }
