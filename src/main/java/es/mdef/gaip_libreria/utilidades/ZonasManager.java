a53882b0cb9aed8226a1b3ece9dec6465cd28114
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-10-19 18:50:14 +0200

    cambio de list a set

--- src/main/java/es/mdef/gaip_libreria/utilidades/ZonasManager.java
+++ src/main/java/es/mdef/gaip_libreria/utilidades/ZonasManager.java
@@ -3,14 +3,14 @@ package es.mdef.gaip_libreria.utilidades;
 import es.mdef.gaip_libreria.zonas.Localidad;
 import es.mdef.gaip_libreria.zonas.Zona;
 
-import java.util.List;
+import java.util.Set;
 
 public final class ZonasManager {
     private ZonasManager() {
     }
 
     public static void asignarCoordenadas(Zona zona) {
-        List<Localidad> localidades = zona.getLocalidades();
+        Set<Localidad> localidades = zona.getLocalidades();
         double[] inicioFilas = zona.getXInicioFilas();
         double xActual = inicioFilas[0];
         double yActual = zona.getYInicioColumna();
