4412fdad5a48ff79e772bc94b1756367a45f4b12
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-10-28 11:10:33 +0200

    Cambio a role

--- src/main/java/es/mdef/gaip_libreria/constantes/Role.java
+++ src/main/java/es/mdef/gaip_libreria/constantes/Role.java
@@ -1,5 +1,23 @@
 package es.mdef.gaip_libreria.constantes;
 
+import lombok.Getter;
+
+@Getter
 public enum Role {
-    ADMIN, GESTOR_UNIDAD, ANFITRION, PROTOCOLO, CONTROL
+    ADMIN("ROLE_ADMIN"),
+    ROLE_GESTOR_UNIDAD("ROLE_GESTOR_UNIDAD"),
+    ROLE_ANFITRION("ROLE_ANFITRION"),
+    ROLE_PROTOCOLO("ROLE_PROTOCOLO"),
+    ROLE_CONTROL("ROLE_CONTROL");
+
+    private final String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+    
+    @Override
+    public String toString() {
+        return role;
+    }
 }
