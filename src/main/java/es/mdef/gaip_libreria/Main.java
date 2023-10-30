ff7e12b3ed21936a968808af35bead1250aa3f72
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-10-30 18:22:13 +0100

    Añado tipo de zona Parking

--- src/main/java/es/mdef/gaip_libreria/Main.java
+++ src/main/java/es/mdef/gaip_libreria/Main.java
@@ -2,6 +2,7 @@ package es.mdef.gaip_libreria;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+
+        System.out.println("Hola mundo cruel!");
     }
 }
--- src/main/java/es/mdef/gaip_libreria/constantes/TipoDeZona.java
+++ src/main/java/es/mdef/gaip_libreria/constantes/TipoDeZona.java
@@ -1,5 +1,5 @@
 package es.mdef.gaip_libreria.constantes;
 
 public enum TipoDeZona {
-    TRIBUNA, ACOTADO, GENERICA, OTROS, LISTA_DE_ESPERA
+    TRIBUNA, ACOTADO, GENERICA, OTROS, LISTA_DE_ESPERA, PARKING
 }
