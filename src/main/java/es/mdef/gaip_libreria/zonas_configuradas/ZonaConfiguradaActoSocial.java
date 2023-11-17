088995695f97bbb181b8429ac93a7be57ba69567
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-11-17 18:46:29 +0100

    Sobreescribo metodo para obtener el numero de localidades normales

--- src/main/java/es/mdef/gaip_libreria/zonas_configuradas/ZonaConfiguradaActoSocial.java
+++ src/main/java/es/mdef/gaip_libreria/zonas_configuradas/ZonaConfiguradaActoSocial.java
@@ -1,5 +1,10 @@
 package es.mdef.gaip_libreria.zonas_configuradas;
 
+import es.mdef.gaip_libreria.constantes.EstadoDeUnaLocalidad;
+
+import static es.mdef.gaip_libreria.constantes.EstadoLocalidad.NORMAL;
+import static es.mdef.gaip_libreria.constantes.EstadoLocalidad.RESERVADA;
+
 public interface ZonaConfiguradaActoSocial extends ZonaConfigurada {
     int getLocalidadesReservadas();
 
@@ -8,5 +13,11 @@ public interface ZonaConfiguradaActoSocial extends ZonaConfigurada {
     @Override
     default int getNumeroLocalidadesParaRepartir() {
         return getLocalidadesNormales();
+    }
+
+    @Override
+    default int getNumeroLocalidadesPorEstado(EstadoDeUnaLocalidad estado) {
+        return estado == RESERVADA ? getLocalidadesReservadas() :
+                estado == NORMAL ? getLocalidadesNormales() : 0;
     }
 }
