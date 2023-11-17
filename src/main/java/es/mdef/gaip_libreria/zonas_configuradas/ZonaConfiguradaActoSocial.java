c18d2a1398eb346400fc0e63c708b894147f2bee
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-11-17 12:06:50 +0100

    Añado interfaz ZonaConfiguradaActoSocial

---
+++ src/main/java/es/mdef/gaip_libreria/zonas_configuradas/ZonaConfiguradaActoSocial.java
@@ -0,0 +1,7 @@
+package es.mdef.gaip_libreria.zonas_configuradas;
+
+public interface ZonaConfiguradaActoSocial extends ZonaConfigurada {
+    int getLocalidadesReservadas();
+
+    int getLocalidadesNormales();
+}
